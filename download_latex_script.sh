568fb313aa22ddb5c7acf5e17a4ee078f8288dbe
arne <arnevdk@hotmail.com>
2018-06-10 14:43:52 +0200

    update gitlab ci conf

--- download_latex_script.sh
+++ download_latex_script.sh
@@ -1,3 +1,5 @@
 # Download the command wrapper and make it executable
+sudo apt update
+sudo apt install wget
 wget https://raw.githubusercontent.com/blang/latex-docker/master/latexdockercmd.sh
 chmod +x latexdockercmd.sh
